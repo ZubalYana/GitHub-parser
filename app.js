086b6a6dd62301da0c8615f18adb79325a1f4106
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-08-10 15:33:08 +0300

    run server

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express();
+const PORT = 3000;
+const axios = require('axios')
+const TOKEN = ''
+app.listen(PORT, ()=>{
+    console.log(`Sever works on PORT: ${PORT}`)
+})
