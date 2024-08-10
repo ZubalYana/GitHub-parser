928bfa7b4807182ebf211df672b6c8eec1c2a878
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-08-10 15:36:14 +0300

    add the basic heading and fonts

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const PORT = 3000;
 const axios = require('axios')
 const TOKEN = ''
+const path = require('path')
+app.use(express.static(path.join(__dirname, 'public')))
 app.listen(PORT, ()=>{
     console.log(`Sever works on PORT: ${PORT}`)
 })
